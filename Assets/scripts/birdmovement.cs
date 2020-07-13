e4a03005f5d8ad8a3745c3f2b6efc5a86a8c03dc
liamtheriot <liamtheriot@outlook.com>
2020-07-13 15:43:35 -0500

    added files

---
+++ Assets/scripts/birdmovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class birdmovement : MonoBehaviour {
+
+    private Rigidbody rb;
+    public float flapforce;
+
+
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.up * flapforce, ForceMode.VelocityChange);
+        }
+	}
+}
---
+++ Assets/scripts/pipemovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pipemovement : MonoBehaviour {
+
+    public float movespeed;
+
+
+	// Use this for initialization
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position += Vector3.back* (movespeed * Time.deltaTime);
+	}
+}
